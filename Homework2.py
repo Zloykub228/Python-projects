96cb0ab06beaa8e1e19e09bbf820cb1af5f28c8b
Admin <danielkunyansky@gmail.com>
2022-11-06 16:46:43 +0200

    Homework2

--- Homework2.py
+++ Homework2.py
@@ -45,25 +45,28 @@ for i in range(365):
     print('Your money-', student.money)
     x = random.randint(1, 3)
     y = random.randint(1, 6)
-    if (student.intelligence == 0 or student.intelligence < 0):
-        print(
-            'Your intelligence is over you can go on work or in university, but until it you will go in university forced without choice to compansate missing value')
-        student.money = student.money + y
-    if (student.money == 0 or student.money < 0):
-        print(
-            'Your money is over you can go on work or in university, but until it you will go on work forced without choice to compansate missing value')
-        student.money = student.money + y
     print('Will you go on work or in university?')
     print('write bellow 1(work) or 2(university)')
     choose = int(input('choose= '))
-    if (choose == 1):
-        student.money = student.money + y
-    if (choose == 2):
-        student.intelligence = student.intelligence + 3
-    student.intelligence = student.intelligence - 2
-    student.money = student.money - x
-    day.day = day.day + 1
-    print('------------------------------------')
+    if (choose==1 or choose==2):
+        if (student.intelligence == 0 or student.intelligence < 0):
+            print('Your intelligence is over you can go on work or in university, but until it you will go in university forced without choice to compansate missing value')
+            student.money = student.money + y
+        if (student.money == 0 or student.money < 0):
+            print('Your money is over you can go on work or in university, but until it you will go on work forced without choice to compansate missing value')
+            student.money = student.money + y
+        if (choose == 2):
+            student.money = student.money + y
+        if (choose == 1):
+            student.intelligence = student.intelligence + 3
+        student.intelligence = student.intelligence - 2
+        student.money = student.money - x
+        day.day = day.day + 1
+        print('------------------------------------')
+    else:
+        time.sleep(1)
+        print('You can choose only "W" or "U"!')
+        time.sleep(1)
 
 print('You did student challenge ')
 
--- Lesson 3.py
+++ Lesson 3.py
@@ -1,3 +1,4 @@
+'''
 try:
     a = int(input('a- '))
     b = int(input('b- '))
@@ -5,3 +6,12 @@ try:
     print('a/b >>> ', a/b)
 except:
     print('На ноль не делится')
+'''
+try:
+    print('Hi start code')
+    print(error)
+    print(125/0)
+    print('no errors...')
+except (NameError, ZeroDivisionError):
+    print('We have an error...'
+          'or we have an ZerodivisionError...')
